dac538f4635d10d48a29004b68c9bd72e835fa87
Oliver <opoku.oliver@gmail.com>
2017-04-18 19:09:50 +0200

    Emoji Commit

--- Emoji/ViewController.swift
+++ Emoji/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var EmojiView: UITableView!
+    
+    var emojilist = ["😱","😟","😜","😡","😨","😓","🙏🏿", "💀"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        EmojiView.dataSource = self
+        EmojiView.delegate = self
+        
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojilist.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        print (indexPath.row)
+        cell.textLabel?.text = emojilist [indexPath.row]
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
